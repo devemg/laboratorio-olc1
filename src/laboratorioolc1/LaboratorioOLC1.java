d2efdd37b335c06025db26ab400f3adf5bd5f435
Emely Garcia <garciam.emm@gmail.com>
2021-04-09 08:38:50 -0600

    initial

---
+++ src/laboratorioolc1/LaboratorioOLC1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorioolc1;
+
+/**
+ *
+ * @author gm_ye
+ */
+public class LaboratorioOLC1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
